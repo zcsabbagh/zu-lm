91dddb1c73bb3868c44d68839876b8fc5ac197d9
uche-afk <112139467+ucheochuba@users.noreply.github.com>
2025-02-15 00:06:57 -0800

    create cargo.toml

---
+++ researcher/target/debug/deps/researcher-e560614b277126aa.d
@@ -0,0 +1,5 @@
+/Users/uochuba/Documents/GitHub/zu-lm/researcher/target/debug/deps/libresearcher-e560614b277126aa.rmeta: src/main.rs
+
+/Users/uochuba/Documents/GitHub/zu-lm/researcher/target/debug/deps/researcher-e560614b277126aa.d: src/main.rs
+
+src/main.rs:
